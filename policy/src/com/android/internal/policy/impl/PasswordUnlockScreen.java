844591967fc0596f27e4c391c925ce145bdd7e54
Danesh M <daneshm90@gmail.com>
2012-01-29 23:45:50 -0500

    Lockscreen : Quick / menu unlock (Danesh M)
    Lockscreen : Quick / menu unlock
    
    Unlock password/pin lock screen without pressing enter button
    
    Patchset 2 : Remove unecessary intialization
    
    Change-Id: Iecb5ed52ef28234f6adad813906dc885296a157c
    
    Quick unlock : Fix bug
    
    There is an issue where users whose passwords are greater than 8 chars
    and
    have quick unlock enabled will be unable to unlock theirlock screenn.
    This is due to the logic evaluating anything beyond 3chars as a
    password attempt.
    
    This change modifies the quick unlock behavior so that when typing the
    password,
    each character is considered an attempt, however if incorrect, is not
    reported as a failed attempt.
    Only when the user presses enter is it considered an "explicit" attempt
    and a failed attempt is reported.
    
    Patchset 2 : Rename runnable to better reflect its purpose
    Patchset 3 : Switch to new behavior
    Patchset 4 : Fix padding
    
    Change-Id: Ida6289c77f6724cb42e8d3b82593f29afe3d6d1b
    
    Menu unlock (frameworks part)
    
        	The option in Settings/Security/Lockscreen was
        	added before in some CM commit, but not working
        	and was missing a proper frameworks part
    
    Patchet 3: Done like master Danesh M wanted
    
    Change-Id: I589c77cd58d7beb4093e334f074c7f7643d90858
    
    Change-Id: Ida6289c77f6724cb42e8d3b82593f29afe3d6d1b

--- policy/src/com/android/internal/policy/impl/PasswordUnlockScreen.java
+++ policy/src/com/android/internal/policy/impl/PasswordUnlockScreen.java
@@ -71,6 +71,7 @@ public class PasswordUnlockScreen extends LinearLayout implements KeyguardScreen
 
     private final KeyguardStatusViewManager mStatusViewManager;
     private final boolean mUseSystemIME = true; // TODO: Make configurable
+    private boolean mQuickUnlock;
     private boolean mResuming; // used to prevent poking the wakelock during onResume()
 
     // To avoid accidental lockout due to events while the device in in the pocket, ignore
@@ -158,6 +159,10 @@ public class PasswordUnlockScreen extends LinearLayout implements KeyguardScreen
                 mCallback.pokeWakelock();
             }
         });
+
+        mQuickUnlock = (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL, 0) == 1);
+
         mPasswordEntry.addTextChangedListener(new TextWatcher() {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
@@ -168,6 +173,14 @@ public class PasswordUnlockScreen extends LinearLayout implements KeyguardScreen
             public void afterTextChanged(Editable s) {
                 if (!mResuming) {
                     mCallback.pokeWakelock();
+                }
+                if (mQuickUnlock) {
+                    String entry = mPasswordEntry.getText().toString();
+                    if (entry.length() > MINIMUM_PASSWORD_LENGTH_BEFORE_REPORT &&
+                            mLockPatternUtils.checkPassword(entry)) {
+                            mCallback.keyguardDone(true);
+                            mCallback.reportSuccessfulUnlockAttempt();
+                    }
                 }
             }
         });
