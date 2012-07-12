32d600862fbfd760ab08a972ef382c8bcfe0fe39
Roman Birg <romanbirg@gmail.com>
2012-07-12 14:36:11 -0500

    SystemUI: toggles WIP

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -967,4 +967,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         KeyguardManager km = (KeyguardManager) mContext.getSystemService(Context.KEYGUARD_SERVICE);
         return km.inKeyguardRestrictedInputMode();
     }
+
+    public boolean isTablet() {
+        return false;
+    }
 }
