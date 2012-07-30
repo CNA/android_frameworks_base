1c5692d00957051ce2b3ef8b38214211096ed929
blunden <blunden2@gmail.com>
2012-07-30 15:31:21 +0200

    Add option to always show battery status on lock screen (1/2)
    Change-Id: I19c89abe686a3636698a816fb78bbbe998c81c89

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2687,6 +2687,12 @@ public final class Settings {
         public static final String LOCKSCREEN_CALENDAR_REMINDERS_ONLY = "lockscreen_calendar_reminders_only";
 
         /**
+         * Always show the battery status on the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_ALWAYS_SHOW_BATTERY = "lockscreen_always_show_battery";
+
+        /**
          * Show the pending notification counts as overlays on the status bar
          * @hide
          */
@@ -2803,6 +2809,7 @@ public final class Settings {
             QUIET_HOURS_DIM,
             SYSTEM_PROFILES_ENABLED,
             POWER_MENU_SCREENSHOT_ENABLED,
+            LOCKSCREEN_ALWAYS_SHOW_BATTERY,
         };
 
         // Settings moved to Settings.Secure
