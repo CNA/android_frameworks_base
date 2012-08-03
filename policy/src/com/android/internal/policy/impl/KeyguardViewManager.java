4401d4b66e544e9239cb98e41993767cd66dd61b
Codenamedroid <codenamegit@gmail.com>
2012-08-03 12:19:13 -0500

    Revert "Add theming of lockscreen"
    This reverts commit 5d7f0b008e0d993253bf85422309459f75ceace9.

--- policy/src/com/android/internal/policy/impl/KeyguardViewManager.java
+++ policy/src/com/android/internal/policy/impl/KeyguardViewManager.java
@@ -18,12 +18,8 @@ package com.android.internal.policy.impl;
 
 import com.android.internal.R;
 
-import com.android.internal.app.ThemeUtils;
-
 import android.app.ActivityManager;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Resources;
 import android.graphics.PixelFormat;
@@ -50,7 +46,6 @@ public class KeyguardViewManager implements KeyguardWindowController {
     private static String TAG = "KeyguardViewManager";
 
     private final Context mContext;
-    private Context mUiContext;
     private final ViewManager mViewManager;
     private final KeyguardViewCallback mCallback;
     private final KeyguardViewProperties mKeyguardViewProperties;
@@ -78,24 +73,11 @@ public class KeyguardViewManager implements KeyguardWindowController {
             KeyguardViewCallback callback, KeyguardViewProperties keyguardViewProperties,
             KeyguardUpdateMonitor updateMonitor) {
         mContext = context;
-        ThemeUtils.registerThemeChangeReceiver(mContext, new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                mUiContext = null;
-            }
-        });
         mViewManager = viewManager;
         mCallback = callback;
         mKeyguardViewProperties = keyguardViewProperties;
 
         mUpdateMonitor = updateMonitor;
-    }
-
-    private Context getUiContext() {
-        if (mUiContext == null) {
-            mUiContext = ThemeUtils.createUiContext(mContext);
-        }
-        return mUiContext != null ? mUiContext : mContext;
     }
 
     /**
@@ -123,14 +105,14 @@ public class KeyguardViewManager implements KeyguardWindowController {
     public synchronized void show() {
         if (DEBUG) Log.d(TAG, "show(); mKeyguardView==" + mKeyguardView);
 
-        Resources res = getUiContext().getResources();
+        Resources res = mContext.getResources();
         boolean enableScreenRotation =
                 SystemProperties.getBoolean("lockscreen.rot_override",false)
                 || res.getBoolean(R.bool.config_enableLockScreenRotation);
         if (mKeyguardHost == null) {
             if (DEBUG) Log.d(TAG, "keyguard host is null, creating it...");
 
-            mKeyguardHost = new KeyguardViewHost(getUiContext(), mCallback);
+            mKeyguardHost = new KeyguardViewHost(mContext, mCallback);
 
             final int stretch = ViewGroup.LayoutParams.MATCH_PARENT;
             int flags = WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN
@@ -175,7 +157,7 @@ public class KeyguardViewManager implements KeyguardWindowController {
 
         if (mKeyguardView == null) {
             if (DEBUG) Log.d(TAG, "keyguard view is null, creating it...");
-            mKeyguardView = mKeyguardViewProperties.createKeyguardView(getUiContext(), mCallback,
+            mKeyguardView = mKeyguardViewProperties.createKeyguardView(mContext, mCallback,
                     mUpdateMonitor, this);
             mKeyguardView.setId(R.id.lock_screen);
 
