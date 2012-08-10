47aff08fdd1ba8b640d207d0b3a58725fdc9d9d3
StevenHarperUK <stevenharperuk@gmail.com>
2012-08-11 00:17:35 +0100

    Lockscreen Vibrate option 1 of 2
    Lockscreen Vibrate option 1 of 2
    
    The new System setting Settings.System.LOCKSCREEN_VIBRATE_ENABLED
    which defaults to 1, will effect the lock screen on touch event
    (when touching the unlock icon - or sliding it).
    
    The GlowPadView (Slider) now has no Vibrate when the user selects
    thebehaviorr
    in the Lockscreen Settings (see 2 of 2 patch).

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2253,6 +2253,12 @@ public final class Settings {
         public static final String LOCKSCREEN_DISABLED = "lockscreen.disabled";
 
         /**
+         * Whether the lockscreen vibrate should be enabled.
+         * @hide
+         */
+        public static final String LOCKSCREEN_VIBRATE_ENABLED = "lockscreen.vibrate_enabled";
+
+        /**
          * Stores values for custom lockscreen targets
          * @hide
          */
@@ -2883,6 +2889,7 @@ public final class Settings {
             SYSTEM_PROFILES_ENABLED,
             POWER_MENU_SCREENSHOT_ENABLED,
             LOCKSCREEN_ALWAYS_SHOW_BATTERY,
+            LOCKSCREEN_VIBRATE_ENABLED,
         };
 
         // Settings moved to Settings.Secure
