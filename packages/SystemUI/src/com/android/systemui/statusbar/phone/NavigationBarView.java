23da43153e318a8bfc189836917d073199d603e5
Codenamedroid <bemegadroid@gmail.com>
2012-08-13 14:47:05 -0500

    bring back default behavior of back key when an IME is displayed

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -476,11 +476,10 @@ public class NavigationBarView extends LinearLayout {
         if (getBackButton() != null) {
         	getBackButton().setAlpha(
         			(0 != (hints & StatusBarManager.NAVIGATION_HINT_BACK_NOP)) ? 0.5f : 1.0f);
-        	// Disable this for the moment
-        	/*((ImageView)getBackButton()).setImageDrawable(
+        	((ImageView)getBackButton()).setImageDrawable(
                 (0 != (hints & StatusBarManager.NAVIGATION_HINT_BACK_ALT))
                     ? (mVertical ? mBackAltLandIcon : mBackAltIcon)
-                    : (mVertical ? mBackLandIcon : mBackIcon)); */
+                    : (mVertical ? mBackLandIcon : mBackIcon));
         }
         if (getHomeButton()!=null) {
         	getHomeButton().setAlpha(
