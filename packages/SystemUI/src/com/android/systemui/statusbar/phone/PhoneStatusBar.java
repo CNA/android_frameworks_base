a1b3c57d33142361f961885c8d7e1c340e7e24ec
Codenamedroid <bemegadroid@gmail.com>
2012-08-25 17:31:03 -0500

    long press status bar settings button to enable/disable the power widget

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -66,6 +66,9 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
@@ -73,6 +76,7 @@ import android.view.WindowManager;
 import android.view.WindowManagerImpl;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
@@ -296,6 +300,8 @@ public class PhoneStatusBar extends BaseStatusBar {
                     Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.SCREEN_BRIGHTNESS_MODE), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.EXPANDED_VIEW_WIDGET), false, this);
             update();
         }
 
@@ -523,6 +529,7 @@ public class PhoneStatusBar extends BaseStatusBar {
 
         mScrollView = (ScrollView)mStatusBarWindow.findViewById(R.id.scroll);
         mScrollView.setVerticalScrollBarEnabled(false); // less drawing during pulldowns
+        mSettingsButton.setOnLongClickListener(mSettingsLongClickListener);
 
         mPowerWidget = (PowerWidget)mStatusBarWindow.findViewById(R.id.exp_power_stat);
         mPowerWidget.setGlobalButtonOnClickListener(new View.OnClickListener() {
@@ -593,6 +600,10 @@ public class PhoneStatusBar extends BaseStatusBar {
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         context.registerReceiver(mBroadcastReceiver, filter);
 
+        //check for view when not pressed on settings button
+        boolean whatIsIt = (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.EXPANDED_VIEW_WIDGET, 1) == 1);
+        mPowerWidget.setVisibility(whatIsIt ? View.VISIBLE : View.GONE);
         mPowerWidget.setupWidget();
         return mStatusBarView;
     }
@@ -2381,6 +2392,56 @@ public class PhoneStatusBar extends BaseStatusBar {
             v.getContext().startActivity(new Intent(Settings.ACTION_SETTINGS)
                     .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
             animateCollapse();
+        }
+    };
+
+    private View.OnLongClickListener mSettingsLongClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+                if (mPowerWidget.getVisibility() == View.GONE) {
+                        int height = mPowerWidget.getHeight();
+                        Animation anim = AnimationUtils.makeInAnimation(mContext, true);
+                        anim.setDuration(500);
+                        anim.setAnimationListener(new AnimationListener() {
+                                @Override
+                                public void onAnimationStart(Animation animation) {
+                                        mPowerWidget.setVisibility(View.VISIBLE);
+                                        Settings.System.putInt(mContext.getContentResolver(), Settings.System.EXPANDED_VIEW_WIDGET, 1);
+                                }
+                                //stupid android wont compile empty methods so I have to override them to work.... better make them public too!
+                                @Override
+                                public void onAnimationEnd(Animation animation) {
+
+                                }
+                                @Override
+                                public void onAnimationRepeat(Animation animation) {
+
+                                }
+                        });
+                        mPowerWidget.startAnimation(anim);
+                } else {
+                        int height = mPowerWidget.getHeight();
+                        Animation anim = AnimationUtils.makeOutAnimation(mContext, false);
+                        anim.setDuration(500);
+                        anim.setAnimationListener(new AnimationListener() {
+                                @Override
+                                public void onAnimationEnd(Animation animation) {
+                                        mPowerWidget.setVisibility(View.GONE);
+                                        Settings.System.putInt(mContext.getContentResolver(), Settings.System.EXPANDED_VIEW_WIDGET, 0);
+                                }
+                                //stupid android wont compile empty methods so I have to override them to work....
+                                @Override
+                                public void onAnimationStart(Animation animation) {
+
+                                }
+                                @Override
+                                public void onAnimationRepeat(Animation animation) {
+
+                                }
+                        });
+                        mPowerWidget.startAnimation(anim);
+                }
+                return true;
         }
     };
 
